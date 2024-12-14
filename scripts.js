75ba83f392d2fc480e1084ce2ffa6f9df972246a
Ayres Roux <ayresrouxio@gmail.com>
2024-12-14 16:54:50 -0300

    aula: exibindo o footer

--- scripts.js
+++ scripts.js
@@ -1,7 +1,13 @@
+// Cotação de moedas do dia.
+const USD = 4.87
+const EUR = 5.32
+const GBP = 6.08
+
 // Obtendo os elementos do formulário.
 const form = document.querySelector("form")
 const amount = document.querySelector("#amount")
 const currency = document.querySelector("#currency")
+const footer = document.querySelector("main footer")
 
 // Manipulando o input amount para receber somente números.
 amount.addEventListener("input", () => {
@@ -12,5 +18,31 @@ amount.addEventListener("input", () => {
 // Captura o evento de submit (enviar) do formulário.
 form.onsubmit = (event) => {
   event.preventDefault()
-  console.log(currency.value)
+
+  switch (currency.value) {
+    case "USD":
+      convertCurrency(amount.value, USD, "US$")
+      break
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€")
+      break
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£")
+      break
+  }
+}
+
+// Função para converter a moeda.
+function convertCurrency(amount, price, symbol) {
+  try {
+    // Aplicar a classe que exibe o footer para mostrar o resultado.
+    footer.classList.add("show-result")
+  } catch (error) {
+    // Remove a classe do footer removendo ele da tela.
+    footer.classList.remove("show-result")
+
+    console.log(error)
+
+    alert("Não foi possível converter. Tente novamente mais tarde.")
+  }
 }
