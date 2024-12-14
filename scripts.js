65d364bfdfb687a837bcf05ce1474452f120823a
Ayres Roux <ayresrouxio@gmail.com>
2024-12-14 16:29:28 -0300

    aula: obtendo a moeda selecionada

--- scripts.js
+++ scripts.js
@@ -1,7 +1,16 @@
+// Obtendo os elementos do formulário.
+const form = document.querySelector("form")
 const amount = document.querySelector("#amount")
+const currency = document.querySelector("#currency")
 
 // Manipulando o input amount para receber somente números.
 amount.addEventListener("input", () => {
   const hasCharactersRegex = /\D+/g
   amount.value = amount.value.replace(hasCharactersRegex, "")
 })
+
+// Captura o evento de submit (enviar) do formulário.
+form.onsubmit = (event) => {
+  event.preventDefault()
+  console.log(currency.value)
+}
